42954782a782d268baa1cd95a89f62755a0fb07d
Erin Martin <ecm8807@gmail.com>
2025-07-01 16:01:50 -0400

    added homepage hero section and button to dashboard

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -3,4 +3,20 @@
 	// let { notes } = $derived(data)
 </script>
 
-HOME
+<!--NAV BAR-->
+
+<section class="banner">
+	<div class="title-text text-center bg-blue-200 mx-auto px-30 py-5">
+		<h2>MixFolio</h2>
+		<p>
+			Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow's nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters.
+			Deadlights jack lad schooner scallywag dance the hempen jig carouser broadside cable strike colors. Bring a spring upon her cable holystone blow the man down spanker Shiver me timbers to go on account lookout wherry doubloon chase. Belay yo-ho-ho keelhaul squiffy black spot yardarm spyglass sheet transom heave to.
+			Trysail Sail ho Corsair red ensign hulk smartly boom jib rum gangway. Case shot Shiver me timbers gangplank crack Jennys tea cup ballast Blimey lee snow crow's nest rutters. Fluke jib scourge of the seven seas boatswain schooner gaff booty Jack Tar transom spirits.
+		</p>
+		<button class="bg-gray-500 hover:bg-gray-800 text-white m-5 py-2 px-4 rounded">
+			<a href="login">Dashboard</a>
+		</button>
+	</div>
+</section>
+
+<!--FOOTER-->
