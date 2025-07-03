d532f5fe5b5a4fa0fe83285870e2ceb9037acbd6
jakebogan01 <jakebogan01@gmail.com>
2025-07-03 16:39:56 -0400

    created dashbaord ui

--- src/lib/utils/constants.js
+++ src/lib/utils/constants.js
@@ -1,3 +1,7 @@
 // auth
 export const LOGIN = '/login';
 export const REGISTER = '/register';
+
+// dashbaord
+export const DASHBOARD = '/dashboard';
+export const FORM = '/dashboard/form';
--- src/routes/(dashboard)/dashboard/form/+page.svelte
+++ src/routes/(dashboard)/dashboard/form/+page.svelte
@@ -366,7 +366,7 @@
 			<main class="flex-1 overflow-y-auto">
 				<div class="mx-auto max-w-7xl px-4 pt-8 sm:px-6 lg:px-8">
 					<div class="flex">
-						<h1 class="flex-1 text-2xl font-bold text-gray-900">Photos</h1>
+						<h1 class="flex-1 text-2xl font-bold text-gray-900">About</h1>
 						<div class="ml-6 flex items-center rounded-lg bg-gray-100 p-0.5 sm:hidden">
 							<button
 								type="button"
@@ -409,130 +409,45 @@
 						</div>
 					</div>
 
-					<!-- Tabs -->
-					<div class="mt-3 sm:mt-2">
-						<div class="grid grid-cols-1 sm:hidden">
-							<!-- Use an "onChange" listener to redirect the user to the selected tab URL. -->
-							<select
-								aria-label="Select a tab"
-								class="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-2 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
-							>
-								<option selected>Recently Viewed</option>
-								<option>Recently Added</option>
-								<option>Favorited</option>
-							</select>
-							<svg
-								class="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end fill-gray-500"
-								viewBox="0 0 16 16"
-								fill="currentColor"
-								aria-hidden="true"
-								data-slot="icon"
-							>
-								<path
-									fill-rule="evenodd"
-									d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"
-									clip-rule="evenodd"
-								/>
-							</svg>
-						</div>
-						<div class="hidden sm:block">
-							<div class="flex items-center border-b border-gray-200">
-								<nav class="-mb-px flex flex-1 space-x-6 xl:space-x-8" aria-label="Tabs">
-									<!-- Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" -->
-									<a
-										href="#"
-										aria-current="page"
-										class="border-b-2 border-indigo-500 px-1 py-4 text-sm font-medium whitespace-nowrap text-indigo-600"
-										>Recently Viewed</a
-									>
-									<a
-										href="#"
-										class="border-b-2 border-transparent px-1 py-4 text-sm font-medium whitespace-nowrap text-gray-500 hover:border-gray-300 hover:text-gray-700"
-										>Recently Added</a
-									>
-									<a
-										href="#"
-										class="border-b-2 border-transparent px-1 py-4 text-sm font-medium whitespace-nowrap text-gray-500 hover:border-gray-300 hover:text-gray-700"
-										>Favorited</a
-									>
-								</nav>
-								<div class="ml-6 hidden items-center rounded-lg bg-gray-100 p-0.5 sm:flex">
-									<button
-										type="button"
-										class="rounded-md p-1.5 text-gray-400 hover:bg-white hover:shadow-xs focus:ring-2 focus:ring-indigo-500 focus:outline-hidden focus:ring-inset"
-									>
-										<svg
-											class="size-5"
-											viewBox="0 0 20 20"
-											fill="currentColor"
-											aria-hidden="true"
-											data-slot="icon"
-										>
-											<path
-												fill-rule="evenodd"
-												d="M2 3.75A.75.75 0 0 1 2.75 3h14.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 3.75Zm0 4.167a.75.75 0 0 1 .75-.75h14.5a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1-.75-.75Zm0 4.166a.75.75 0 0 1 .75-.75h14.5a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1-.75-.75Zm0 4.167a.75.75 0 0 1 .75-.75h14.5a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1-.75-.75Z"
-												clip-rule="evenodd"
-											/>
-										</svg>
-										<span class="sr-only">Use list view</span>
-									</button>
-									<button
-										type="button"
-										class="ml-0.5 rounded-md bg-white p-1.5 text-gray-400 shadow-xs focus:ring-2 focus:ring-indigo-500 focus:outline-hidden focus:ring-inset"
-									>
-										<svg
-											class="size-5"
-											viewBox="0 0 20 20"
-											fill="currentColor"
-											aria-hidden="true"
-											data-slot="icon"
-										>
-											<path
-												fill-rule="evenodd"
-												d="M4.25 2A2.25 2.25 0 0 0 2 4.25v2.5A2.25 2.25 0 0 0 4.25 9h2.5A2.25 2.25 0 0 0 9 6.75v-2.5A2.25 2.25 0 0 0 6.75 2h-2.5Zm0 9A2.25 2.25 0 0 0 2 13.25v2.5A2.25 2.25 0 0 0 4.25 18h2.5A2.25 2.25 0 0 0 9 15.75v-2.5A2.25 2.25 0 0 0 6.75 11h-2.5Zm9-9A2.25 2.25 0 0 0 11 4.25v2.5A2.25 2.25 0 0 0 13.25 9h2.5A2.25 2.25 0 0 0 18 6.75v-2.5A2.25 2.25 0 0 0 15.75 2h-2.5Zm0 9A2.25 2.25 0 0 0 11 13.25v2.5A2.25 2.25 0 0 0 13.25 18h2.5A2.25 2.25 0 0 0 18 15.75v-2.5A2.25 2.25 0 0 0 15.75 11h-2.5Z"
-												clip-rule="evenodd"
-											/>
-										</svg>
-										<span class="sr-only">Use grid view</span>
-									</button>
-								</div>
-							</div>
-						</div>
-					</div>
-
 					<!-- Gallery -->
 					<section class="mt-8 pb-16" aria-labelledby="gallery-heading">
-						<h2 id="gallery-heading" class="sr-only">Recently viewed</h2>
-						<ul
-							role="list"
-							class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 md:grid-cols-4 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8"
-						>
-							{#each Array(20) as _}
-								<li class="relative">
-									<!-- Current: "ring-2 ring-indigo-500 ring-offset-2", Default: "focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100" -->
+						<h2 id="gallery-heading" class="sr-only">Form</h2>
+						<div class="flex items-start space-x-4">
+							<div class="min-w-0 flex-1">
+								<form action="#" class="relative">
 									<div
-										class="group block w-full overflow-hidden rounded-lg bg-gray-100 ring-2 ring-indigo-500 ring-offset-2"
+										class="rounded-lg bg-white outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600"
 									>
-										<!-- Current: "", Default: "group-hover:opacity-75" -->
-										<img
-											src="https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80"
-											alt=""
-											class="pointer-events-none aspect-10/7 object-cover"
-										/>
-										<button type="button" class="absolute inset-0 focus:outline-hidden">
-											<span class="sr-only">View details for IMG_4985.HEIC</span>
-										</button>
+										<label for="comment" class="sr-only">Add your comment</label>
+										<textarea
+											rows="3"
+											name="comment"
+											id="comment"
+											class="block w-full resize-none bg-transparent px-3 py-1.5 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+											placeholder="Add your comment..."
+										></textarea>
+
+										<!-- Spacer element to match the height of the toolbar -->
+										<div class="py-2" aria-hidden="true">
+											<!-- Matches height of button in toolbar (1px border + 36px content height) -->
+											<div class="py-px">
+												<div class="h-9"></div>
+											</div>
+										</div>
 									</div>
-									<p
-										class="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900"
-									>
-										IMG_4985.HEIC
-									</p>
-									<p class="pointer-events-none block text-sm font-medium text-gray-500">3.9 MB</p>
-								</li>
-							{/each}
-							<!-- More files... -->
-						</ul>
+
+									<div class="absolute inset-x-0 bottom-0 flex justify-between py-2 pr-2 pl-3">
+										<div class="shrink-0">
+											<button
+												type="submit"
+												class="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+												>Post</button
+											>
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
 					</section>
 				</div>
 			</main>
