792096b081fe07815ad74acbad9abd0b431e502f
Erin Martin <ecm8807@gmail.com>
2025-07-10 15:02:16 -0400

    created cookie util

--- src/routes/(dashboard)/dashboard/+layout.server.js
+++ src/routes/(dashboard)/dashboard/+layout.server.js
@@ -3,3 +3,9 @@
  * directory. It makes the routes in this directory _dynamic_ routes, which
  * send a server request, and thus trigger `hooks.server.ts`.
  **/
+import { readSuccessMessage } from '$lib/utils/cookieMessage.js';
+
+export const load = async ({ cookies }) => {
+	const successMessage = readSuccessMessage(cookies);
+	return { successMessage };
+};
