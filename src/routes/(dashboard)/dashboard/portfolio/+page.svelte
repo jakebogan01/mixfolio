b6a63b0a4a90aa9cf2fecb1a7825ded9ee755ec4
jakebogan01 <jakebogan01@gmail.com>
2025-07-11 14:50:23 -0400

    updates data flow

--- src/routes/(dashboard)/dashboard/portfolio/+page.svelte
+++ src/routes/(dashboard)/dashboard/portfolio/+page.svelte
@@ -25,17 +25,17 @@
 <Nav />
 
 <div class="mx-auto max-w-7xl px-6">
-	{#if user_profile.length > 0}
+	{#if Object.keys(user_profile).length !== 0}
 		<div class="mb-10">
-			<h2 class="text-2xl font-bold">User Profile</h2>
+			<h2 class="text-2xl font-bold">Your Profile</h2>
 			<div class="flow-root">
 				<div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
 					<div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
 						<table class="min-w-full divide-y divide-gray-300">
 							<colgroup>
-								<col class="w-full sm:w-2/12" />
-								<col class="lg:w-2/12" />
-								<col class="lg:w-2/12" />
+								<col class="w-full sm:w-1/12" />
+								<col class="lg:w-1/12" />
+								<col class="lg:w-1/12" />
 								<col class="lg:w-1/12" />
 								<col class="lg:w-1/12" />
 								<col class="lg:w-1/12" />
@@ -76,6 +76,11 @@
 										class="py-3.5 pr-3 pl-4 text-left text-sm font-semibold text-gray-900 sm:pl-0"
 										>Address</th
 									>
+									<th
+										scope="col"
+										class="py-3.5 pr-3 pl-4 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+										>URL</th
+									>
 									<th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
 										>Bio</th
 									>
@@ -91,87 +96,89 @@
 								</tr>
 							</thead>
 							<tbody class="divide-y divide-gray-200">
-								{#each user_profile as item (item?.id)}
-									<tr>
-										<td
-											class="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0"
-											>{item?.id}</td
+								<tr>
+									<td
+										class="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0"
+										>{user_profile?.id}</td
+									>
+									<td
+										class="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0"
+										>{user_profile?.name}</td
+									>
+									<td
+										class="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0"
+										>{user_profile?.email}</td
+									>
+									<td
+										class="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0"
+										>{user_profile?.phone}</td
+									>
+									<td
+										class="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0"
+										>{user_profile?.role}</td
+									>
+									<td
+										class="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0"
+										>{user_profile?.address}</td
+									>
+									<td
+										class="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0"
+										>mixfolio.com/{user_profile?.user_preferences?.slug}</td
+									>
+									<td
+										class="w-40 max-w-xs truncate px-3 py-4 text-sm whitespace-nowrap text-gray-500"
+										>{user_profile?.bio}</td
+									>
+									<td class="px-3 py-4 text-sm whitespace-nowrap text-gray-500"
+										>{toISODate(user_profile?.created_at)}</td
+									>
+									<td
+										class="relative py-4 pr-4 pl-3 text-right text-sm font-medium whitespace-nowrap sm:pr-0"
+									>
+										<button
+											type="button"
+											aria-label="Edit record"
+											class="cursor-pointer text-indigo-600 hover:text-indigo-900"
 										>
-										<td
-											class="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0"
-											>{item?.name}</td
-										>
-										<td
-											class="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0"
-											>{item?.email}</td
-										>
-										<td
-											class="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0"
-											>{item?.phone}</td
-										>
-										<td
-											class="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0"
-											>{item?.role}</td
-										>
-										<td
-											class="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0"
-											>{item?.address}</td
-										>
-										<td
-											class="w-40 max-w-xs truncate px-3 py-4 text-sm whitespace-nowrap text-gray-500"
-											>{item?.bio}</td
-										>
-										<td class="px-3 py-4 text-sm whitespace-nowrap text-gray-500"
-											>{toISODate(item?.created_at)}</td
-										>
-										<td
-											class="relative py-4 pr-4 pl-3 text-right text-sm font-medium whitespace-nowrap sm:pr-0"
-										>
-											<button
-												type="button"
-												aria-label="Edit record"
-												class="cursor-pointer text-indigo-600 hover:text-indigo-900"
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												fill="none"
+												viewBox="0 0 24 24"
+												stroke-width="1.5"
+												stroke="currentColor"
+												class="size-5"
+												><path
+													stroke-linecap="round"
+													stroke-linejoin="round"
+													d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+												/></svg
 											>
-												<svg
-													xmlns="http://www.w3.org/2000/svg"
-													fill="none"
-													viewBox="0 0 24 24"
-													stroke-width="1.5"
-													stroke="currentColor"
-													class="size-5"
-													><path
-														stroke-linecap="round"
-														stroke-linejoin="round"
-														d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-													/></svg
-												>
-											</button>
-										</td>
-										<td
-											class="relative py-4 pr-4 pl-3 text-right text-sm font-medium whitespace-nowrap sm:pr-0"
+										</button>
+									</td>
+									<td
+										class="relative py-4 pr-4 pl-3 text-right text-sm font-medium whitespace-nowrap sm:pr-0"
+									>
+										<button
+											type="button"
+											aria-label="Delete record"
+											class="cursor-pointer text-red-600 hover:text-red-400"
 										>
-											<button
-												type="button"
-												aria-label="Delete record"
-												class="cursor-pointer text-red-600 hover:text-red-400"
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												fill="none"
+												viewBox="0 0 24 24"
+												stroke-width="1.5"
+												stroke="currentColor"
+												class="size-5"
+												><path
+													stroke-linecap="round"
+													stroke-linejoin="round"
+													d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+												/></svg
 											>
-												<svg
-													xmlns="http://www.w3.org/2000/svg"
-													fill="none"
-													viewBox="0 0 24 24"
-													stroke-width="1.5"
-													stroke="currentColor"
-													class="size-5"
-													><path
-														stroke-linecap="round"
-														stroke-linejoin="round"
-														d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-													/></svg
-												>
-											</button>
-										</td>
-									</tr>
-								{/each}
+										</button>
+									</td>
+								</tr>
 							</tbody>
 						</table>
 					</div>
@@ -209,6 +216,25 @@
 					class="w-full rounded-md"
 				/>
 			</div>
+
+			<div>
+				<label for="slug" class="sr-only">URL</label>
+				<div>
+					<div
+						class="flex items-center overflow-hidden rounded-md pl-3 outline-1 -outline-offset-1 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600"
+					>
+						<div class="shrink-0 text-gray-500 select-none">mixfolio.com/</div>
+						<input
+							type="text"
+							name="slug"
+							id="slug"
+							class="focus:border-l-none block min-w-0 grow rounded-tr-md rounded-br-md border-l-0 pl-0 placeholder:!text-gray-400 focus:ring-0"
+							placeholder="erin-martin"
+						/>
+					</div>
+				</div>
+			</div>
+
 			<div>
 				<label for="bio" class="sr-only">Bio</label>
 				<textarea name="bio" id="bio" rows="3" placeholder="About me..." class="w-full rounded-md"
--- src/routes/(portfolio)/portfolio/[slug]/+page.svelte
+++ src/routes/(portfolio)/portfolio/[slug]/+page.svelte
@@ -15,23 +15,33 @@
 		<dl class="grid grid-cols-1 sm:grid-cols-2">
 			<div class="border-t border-gray-100 px-4 py-6 sm:col-span-1 sm:px-0">
 				<dt class="text-sm/6 font-medium text-gray-900">Full name</dt>
-				<dd class="mt-1 text-sm/6 text-gray-700 sm:mt-2">Margot Foster</dd>
+				<dd class="mt-1 text-sm/6 text-gray-700 sm:mt-2">{user_profile?.name}</dd>
 			</div>
 			<div class="border-t border-gray-100 px-4 py-6 sm:col-span-1 sm:px-0">
 				<dt class="text-sm/6 font-medium text-gray-900">Phone</dt>
-				<dd class="mt-1 text-sm/6 text-gray-700 sm:mt-2">1 (422) 242-2233</dd>
+				<dd class="mt-1 text-sm/6 text-gray-700 sm:mt-2">{user_profile?.phone}</dd>
 			</div>
 			<div class="border-t border-gray-100 px-4 py-6 sm:col-span-1 sm:px-0">
 				<dt class="text-sm/6 font-medium text-gray-900">Email address</dt>
 				<dd class="mt-1 text-sm/6 text-gray-700 sm:mt-2">{user?.email}</dd>
 			</div>
 			<div class="border-t border-gray-100 px-4 py-6 sm:col-span-1 sm:px-0">
-				<dt class="text-sm/6 font-medium text-gray-900">Salary</dt>
-				<dd class="mt-1 text-sm/6 text-gray-700 sm:mt-2">$120,000</dd>
+				<dt class="text-sm/6 font-medium text-gray-900">Address</dt>
+				<dd class="mt-1 text-sm/6 text-gray-700 sm:mt-2">{user_profile?.address}</dd>
+			</div>
+			<div class="border-t border-gray-100 px-4 py-6 sm:col-span-1 sm:px-0">
+				<dt class="text-sm/6 font-medium text-gray-900">Role</dt>
+				<dd class="mt-1 text-sm/6 text-gray-700 sm:mt-2">{user_profile?.role}</dd>
+			</div>
+			<div class="border-t border-gray-100 px-4 py-6 sm:col-span-1 sm:px-0">
+				<dt class="text-sm/6 font-medium text-gray-900">Slug</dt>
+				<dd class="mt-1 text-sm/6 text-gray-700 sm:mt-2">
+					mixfolio.com/{user_profile?.user_preferences?.slug}
+				</dd>
 			</div>
 			<div class="border-t border-gray-100 px-4 py-6 sm:col-span-2 sm:px-0">
 				<dt class="text-sm/6 font-medium text-gray-900">About</dt>
-				<dd class="mt-1 text-sm/6 text-gray-700 sm:mt-2">{user_profile[0]?.bio}</dd>
+				<dd class="mt-1 text-sm/6 text-gray-700 sm:mt-2">{user_profile?.bio}</dd>
 			</div>
 		</dl>
 	</div>
