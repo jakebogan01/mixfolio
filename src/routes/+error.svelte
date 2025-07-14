39b1b02d27dcf800cbe9547c69ac82ae2ea78830
Erin Martin <ecm8807@gmail.com>
2025-07-14 15:30:28 -0400

    update error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -7,9 +7,17 @@
 	let cleanup, testElement;
 
 	onMount(() => {
+		let source;
+
+		if (page.status === 404) {
+			source = '/404.lottie';
+		} else {
+			source = '/default.lottie';
+		}
+
 		dotLotties = new DotLottie({
 			canvas: testElement,
-			src: '/404.lottie',
+			src: source,
 			autoplay: true,
 			loop: true
 		});
@@ -24,19 +32,20 @@
 <!--<h1>{page.status}: {page.error.message}</h1>-->
 
 <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-	<canvas
+		<canvas
 		bind:this={testElement}
 		class="size-100"
 		draggable="false"
 		aria-label="Paintbrush icon representing web design"
 	></canvas>
-	<div class="text-center">
-		<p class="text-base font-semibold text-indigo-600">Page {page.error.message}</p>
+
+		<div class="text-center">
+<!--		<p class="text-base font-semibold text-indigo-600"> {page.error.message}</p>-->
 		<h1 class="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-			{page.status}
+			OOPS
 		</h1>
 		<p class="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-			Sorry, we couldn’t find the page you’re looking for.
+			Looks like we ran into an error, sorry about that.
 		</p>
 		<div class="mt-10 flex items-center justify-center gap-x-6">
 			<a
