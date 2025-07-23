8b98f4d9bf2e1d40eafa73a98f1b039d33ee73a9
jakebogan01 <jakebogan01@gmail.com>
2025-07-22 22:01:51 -0400

    updated profile quick view and added clients and working on preferences

---
+++ src/lib/components/dashboard/home/ClientsQuickView.svelte
@@ -0,0 +1,56 @@
+<script>
+	let { userProfile } = $props();
+</script>
+
+<div
+	class="relative flex flex-col rounded-xl border border-gray-300 bg-white bg-clip-border text-gray-700"
+>
+	{#if userProfile?.expand?.clients?.length}
+		<div class="relative m-0 overflow-hidden rounded-xl bg-transparent p-6 text-gray-700">
+			<h6 class=" mb-2 block font-sans text-base leading-relaxed font-semibold tracking-normal">
+				My Clients
+			</h6>
+			<p class="mt-2 text-sm text-gray-700">A list of all your clients.</p>
+		</div>
+		<div class="max-h-72 overflow-y-auto p-6 pt-0">
+			<ul role="list" class="divide-y divide-gray-100">
+				{#each userProfile?.expand?.clients as client (client?.id)}
+					<li class="flex items-center justify-between gap-x-6 py-5">
+						<div class="flex min-w-0 gap-x-4">
+							<img
+								src={client?.client_image_url ||
+									'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'}
+								alt=""
+								class="size-12 flex-none rounded bg-gray-50"
+							/>
+							<div class="min-w-0 flex-auto">
+								<p class="text-sm/6 font-semibold text-gray-900">
+									{client?.name || 'Name unavailable'}
+								</p>
+								<p class="mt-1 truncate text-xs/5 text-gray-500">
+									{client?.link || 'Link unavailable'}
+								</p>
+							</div>
+						</div>
+						<a
+							href="/"
+							class="rounded-full bg-gray-900 px-2.5 py-1 text-xs font-semibold text-white sm:transition-colors sm:hover:bg-violet-400"
+							>View</a
+						>
+					</li>
+				{/each}
+			</ul>
+		</div>
+	{:else}
+		<div class="flex h-full min-h-100 items-center justify-center p-6">
+			<div class="flex flex-col items-center space-y-2">
+				<p class="mt-2 text-sm text-gray-700">You do not have any clients.</p>
+				<button
+					type="button"
+					class="block cursor-pointer rounded-md bg-purple-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-xs hover:bg-violet-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+					>Add a client</button
+				>
+			</div>
+		</div>
+	{/if}
+</div>
---
+++ src/lib/components/dashboard/home/DashboardStats.svelte
@@ -0,0 +1,53 @@
+<script>
+	import Icon from '$lib/components/Icon.svelte';
+
+	let numOfPageVisits = 2536;
+	let stats = [
+		{
+			id: 1,
+			title: 'Profile Completion',
+			value: `${53}&percnt;`,
+			icon: 'checkmark'
+		},
+		{
+			id: 2,
+			title: 'Page Visits',
+			value: numOfPageVisits.toLocaleString('en-US'),
+			icon: 'eye'
+		},
+		{
+			id: 3,
+			title: 'Total Projects',
+			value: `${8}/10`,
+			icon: 'stacks'
+		},
+		{
+			id: 4,
+			title: 'Popular Project',
+			value: 'Simple Calculator',
+			icon: 'like'
+		}
+	];
+</script>
+
+<div class="mb-6 grid gap-x-6 gap-y-10 md:grid-cols-2 xl:grid-cols-4">
+	{#each stats as stat (stat.id)}
+		<div
+			class="relative flex flex-col rounded-xl border border-gray-300 bg-white bg-clip-border text-gray-700"
+		>
+			<div
+				class="absolute mx-4 mt-4 grid h-12 w-12 place-items-center overflow-hidden rounded-xl bg-gradient-to-tr from-gray-900 to-gray-800 bg-clip-border text-white"
+			>
+				<Icon name={stat.icon} class="size-6 text-white" stroke="none" />
+			</div>
+			<div class="py-4 pr-4 pl-18 text-right">
+				<p class="block font-sans text-sm leading-normal font-normal">{stat.title}</p>
+				<h4
+					class="ml-auto block max-w-70 truncate font-sans text-2xl leading-snug font-semibold tracking-normal"
+				>
+					{@html stat.value}
+				</h4>
+			</div>
+		</div>
+	{/each}
+</div>
