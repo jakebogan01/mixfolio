a7bc514331db88d15f55718e1b09c400550eef12
Erin Martin <ecm8807@gmail.com>
2025-07-28 11:31:39 -0400

    fixed bugs in portfolio page

--- src/routes/(portfolio)/portfolio/[slug]/+page.js
+++ src/routes/(portfolio)/portfolio/[slug]/+page.js
@@ -1,3 +1,5 @@
+import { processExpandedItems } from '$lib/utils/misc.js';
+
 export const ssr = false;
 
 export async function load({ parent, params }) {
@@ -24,36 +26,61 @@ export async function load({ parent, params }) {
 				'expand.projects.link, ' +
 				'expand.projects.image, ' +
 				'expand.projects.created, ' +
+				'expand.projects.updated, ' +
 				'expand.testimonials.collectionId, ' +
 				'expand.testimonials.id, ' +
 				'expand.testimonials.name, ' +
 				'expand.testimonials.email, ' +
 				'expand.testimonials.role, ' +
+				'expand.testimonials.company, ' +
 				'expand.testimonials.avatar, ' +
 				'expand.clients.collectionId, ' +
 				'expand.clients.id, ' +
 				'expand.clients.name, ' +
 				'expand.clients.link, ' +
-				'expand.clients.image,' +
-				'expand.preferences.id,' +
-				'expand.preferences.portfolio_color',
+				'expand.clients.image, ' +
+				'expand.preferences.id, ' +
+				'expand.preferences.portfolio_color, ' +
+				'expand.preferences.hide_projects, ' +
+				'expand.preferences.hide_testimonials, ' +
+				'expand.preferences.hide_clients, ' +
+				'expand.preferences.hide_portfolio, ' +
+				'expand.preferences.hide_notifications, ' +
+				'expand.preferences.hide_analytics',
 			expand: 'projects, testimonials, clients, preferences'
 		});
-		if (userProfile?.avatar) {
-			let avatar_url = pb.files.getURL(userProfile, userProfile?.avatar);
-			if (avatar_url) userProfile.avatar_url = avatar_url;
-		}
-		if (userProfile?.expand?.projects) {
-			userProfile?.expand?.projects.forEach((item) => {
-				if (item?.image) {
-					let project_image_url = pb.files.getURL(item, item.image);
-					if (project_image_url) item.project_image_url = project_image_url;
-				}
-			});
-		}
-		delete userProfile.collectionId;
-		userProfile.expand.projects.forEach((project) => delete project.collectionId);
+		console.log(userProfile);
 
+		userProfile.avatar_url = userProfile.avatar
+			? pb.files.getURL(userProfile, userProfile.avatar)
+			: null;
+
+		if (userProfile.expand?.projects)
+			userProfile.expand.projects = processExpandedItems(
+				pb,
+				userProfile.expand.projects,
+				'image',
+				'project_image_url'
+			);
+
+		if (userProfile.expand?.testimonials)
+			userProfile.expand.testimonials = processExpandedItems(
+				pb,
+				userProfile.expand.testimonials,
+				'avatar',
+				'testimonial_image_url'
+			);
+
+		if (userProfile.expand?.clients)
+			userProfile.expand.clients = processExpandedItems(
+				pb,
+				userProfile.expand.clients,
+				'image',
+				'client_image_url'
+			);
+
+		if ('collectionId' in userProfile) delete userProfile.collectionId;
+		console.log('Try+Catch',userProfile);
 		return { userProfile: userProfile || {} };
 	} catch (error) {
 		console.dir(error?.message, { depth: null });
