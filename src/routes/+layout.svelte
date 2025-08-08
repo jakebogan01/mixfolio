e645eca5247449b6f37ddc0e31ec6d49acaebf81
jakebogan01 <jakebogan01@gmail.com>
2025-08-08 11:39:33 -0400

    adds analytic data

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
 	import '../app.css';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	injectAnalytics();
+	injectSpeedInsights();
 
 	let { children } = $props();
 </script>
